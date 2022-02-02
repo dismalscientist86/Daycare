2f186d044c4e26a4c4bc79e4474a37224d59877f
dismalscientist86 <danielle.h.sandler@census.gov>
2022-02-02 16:55:21 -0500

    Update cbp_data.R
    Added a loop to get 2017-2019 data

--- cbp_data.R
+++ cbp_data.R
@@ -1,6 +1,6 @@
 library(censusapi)
 apis <- listCensusApis()
-View(apis)
+
 
 cbp.varlist <- c("ESTAB", #Establishments
                  "LFO", # Legal Form of Organization 
@@ -12,12 +12,14 @@ cbp.varlist <- c("ESTAB", #Establishments
 
 all.states<-paste0("state:",paste(sprintf("%02d",c(1,2,4:6,8:13,15:42,44:51,53:56)),collapse=","))
 
-
-cbp <- getCensus(name = "cbp", 
-                      vintage = 2019, 
+cbp18.19<-lapply(2017:last.year,FUN = function(year) 
+          getCensus(name = "cbp", 
+                      vintage = year, 
                       NAICS2017="624410", 
                       region = all.states,
                       vars = cbp.varlist,
                       key = "6065733bac154874a4545105f1e53134719f5680")
+)
 
-View(cbp)
+cbp18.19.2<-do.call(rbind,cbp18.19)
+
